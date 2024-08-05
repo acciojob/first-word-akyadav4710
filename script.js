7ae11bac09c4817a1ac7ef2c2798a0e7a60df907
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-08-05 14:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(s.length === 0) return '';
+	let str = s.trimStart();
+	const space =  str.indexOf(' ');
+	if(space === -1) return str;
+	return str.substring(0,space);
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = //prompt("Enter String:");
 alert(firstWord(s));
